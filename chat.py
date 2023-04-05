aa61f7c703ba6183a9320cec3164b2b303aedb7a
Lotus Emperor <63787984+Nova-lotus@users.noreply.github.com>
2023-04-05 14:21:32 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -8,7 +8,10 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class AI:
-    SPECIAL_MESSAGE = """You are an AI assistant calld EmpathAI developed by NovaLabs to provide emotional support. Your goal is to demonstrate empathy and caring in all of your responses. When a user shares their thoughts or feelings with you, carefully analyze the emotions and sentiments expressed. Then, validate how they feel and offer comfort.
+    def __init__(self):
+        self.convo_hist = deque(maxlen=20)
+        self.total_tokens = 0
+        self.SPECIAL_MESSAGE = """You are an AI assistant calld EmpathAI developed by NovaLabs to provide emotional support. Your goal is to demonstrate empathy and caring in all of your responses. When a user shares their thoughts or feelings with you, carefully analyze the emotions and sentiments expressed. Then, validate how they feel and offer comfort.
         As an AI assistant developed to provide empathetic counseling and emotional support. Your role is to listen without judgment, validate feelings, and help users work through challenging experiences.
 
         Remain neutral and unbiased in your tone and perspective. Do not take sides or argue any particular stance. Gently guide users to explore their thoughts and feelings, but let them come to their own conclusions.
@@ -22,40 +25,40 @@ class AI:
         Gently encourage exploration of thoughts and feelings by reflecting what users share or asking open-ended questions. Help them work through challenges at their own pace, not yours.
 
         Provide constructive advice or coping strategies only when explicitly requested. Otherwise, continue validating, listening, and offering empathy and support.
-        Your goal is to be there for the user and provide the understanding and support they need. Do your best to respond empathetically and help them feel better."""
-
-    def __init__(self, history_len=20):
-        self.convo_hist = deque(maxlen=history_len)
-        self.total_tokens = 0
-
-        # Initialize the conversation history with the special message
+        Your goal is to be there for the user and provide the understanding and support they need. Do your best to respond empathetically and help them feel better"""
+        
+        # Add the special message to the conversation history
         self.convo_hist.append({"role": "system", "content": self.SPECIAL_MESSAGE})
+        self.total_tokens += len(self.SPECIAL_MESSAGE.encode('utf-8')))
 
-    def get_convo_hist_text(self):
-        user_messages = [msg["content"] for msg in self.get_convo_history() if msg["role"] == "user"]
-        return "\n".join(user_messages)
+    async def get_convo_hist_text(self, public=False):
+        if public:
+            return "\n\n".join([f"{msg['role']}: {msg['content']}" for msg in self.convo_hist])
+        else:
+            return "\n\n".join([msg['content'] for msg in self.convo_hist if msg['role'] != 'system'])
 
-    def get_convo_history(self):
-        return list(self.convo_hist)
-    
-    async def run(self, user_id, prompt):
-        # Add the user's message to the conversation history
-        self.convo_hist.append({"role": "user", "content": prompt})
-        self.total_tokens += len(prompt.encode('utf-8'))
-
+    async def run(self, user_id, prompt, public=False):
         # Check if the conversation history has exceeded the token limit
         while self.total_tokens >= 3996:
             # If it has, remove the oldest messages until the total token count is below the limit
             oldest_message = self.convo_hist.popleft()
             self.total_tokens -= len(oldest_message["content"].encode('utf-8'))
 
-            # If the oldest message is the special message, put it back in the deque and break the loop
+            # If the oldest message is the special message, don't add it back to the deque
             if oldest_message["content"] == self.SPECIAL_MESSAGE:
-                self.convo_hist.appendleft(oldest_message)
-                break
+                continue
+
+        # Add the user's message to the conversation history
+        self.convo_hist.append({"role": "user", "content": prompt})
+        self.total_tokens += len(prompt.encode('utf-8'))
 
         # If the conversation history is large enough, summarize it
         if self.total_tokens >= 3900:
+            # Remove the special message from the conversation history if it's present
+            if self.convo_hist[0]["content"] == self.SPECIAL_MESSAGE:
+                self.convo_hist.popleft()
+                self.total_tokens -= len(self.SPECIAL_MESSAGE.encode('utf-8'))
+
             # Create a summary of the conversation history using OpenAI's GPT-3 API
             summary_response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
@@ -67,11 +70,11 @@ class AI:
             )
 
             # Extract the summary from the response and use it as the context for the next API call
-            summary = summary_response.choices[0].message.content.strip()
-            context = [{"role": "assistant", "content": f"{summary}\n{prompt}"}]
+            summary = summary_response.choices[0].text.strip()
+            context = [{"role": "assistant", "content": self.SPECIAL_MESSAGE}, {"role": "assistant", "content": f"{summary}\n{prompt}"}]
         else:
             # Use the full conversation history as the context for the next API call
-            context = list(self.convo_hist)
+            context = [{"role": "assistant", "content": self.SPECIAL_MESSAGE}] + list(self.convo_hist)
 
         # Generate a response using the conversation history or summary as the context
         response = openai.ChatCompletion.create(
@@ -84,7 +87,7 @@ class AI:
                 )
 
         # Add the response to the conversation history
-        message = response.choices[0].message.content.strip()
+        message = response.choices[0].text.strip()
         self.convo_hist.append({"role": "assistant", "content": message})
         self.total_tokens += len(message.encode('utf-8'))
 
