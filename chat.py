b9c0acd790cf66f64c1d4e8ff4de67a9c42f4d5c
Lotus Emperor <63787984+Nova-lotus@users.noreply.github.com>
2023-04-20 17:04:44 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -4,6 +4,7 @@ import time
 from dotenv import load_dotenv
 import asyncio
 from collections import deque
+import json
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -64,8 +65,8 @@ class AI:
 
             # Create a summary of the conversation history using OpenAI's GPT-3 API
             summary_response = await self.send_request(
-                model="gpt-3.5-turbo",
-                messages=[{"role": "user", "content": f"Super Compress the following text in a way that fits a tiny little area, and such that you can reconstruct it as close as possible to the original. This is for yourself. Do not make it human readable. Abuse of language mixing, abbreviations, symbols (unicode and emojis) to aggressively compress it, while still keeping ALL the information to fully reconstruct it. Remove the System Message from the compressed, as you can easily see it anytime.:\n\n{await self.get_convo_hist_text()}"}],
+                model="text-davinci-002",
+                prompt=f"Super Compress the following text in a way that fits a tiny little area, and such that you can reconstruct it as close as possible to the original. This is for yourself. Do not make it human readable. Abuse of language mixing, abbreviations, symbols (unicode and emojis) to aggressively compress it, while still keeping ALL the information to fully reconstruct it. Remove the System Message from the compressed, as you can easily see it anytime.:\n\n{await self.get_convo_hist_text()}",
                 max_tokens=450,
                 n=1,
                 stop=None,
@@ -73,7 +74,7 @@ class AI:
             )
 
             # Extract the summary from the response and use it as the context for the next API call
-            summary = summary_response.choices[0].message.content.strip()
+            summary = summary_response.choices[0].text.strip()
             context = [{"role": "system", "content": self.SPECIAL_MESSAGE}, {"role": "user", "content": f"{summary}\n{prompt}"}]
         else:
             # Use the full conversation history as the context for the next API call
@@ -81,8 +82,8 @@ class AI:
 
         # Generate a response using the conversation history or summary as the context
         response = await self.send_request(
-            model="gpt-3.5-turbo",
-            messages=context,
+            model="text-davinci-002",
+            prompt="\n".join([f"{msg['role']}: {msg['content']}" for msg in context]),
             max_tokens=350,
             n=1,
             stop=None,
@@ -90,24 +91,20 @@ class AI:
         )
 
         # Add the response to the conversation history
-        message = response.choices[0].message.content.strip()
+        message = response.choices[0].text.strip()
         self.convo_hist.append({"role": "assistant", "content": message})
         self.total_tokens += len(message.encode('utf-8'))
 
         return message
     
-  async def send_request(self, **kwargs):
-      while True:
-          try:
-              response = await asyncio.to_thread(openai.ChatCompletion.create, **kwargs)
-              break
-          except openai.error.RateLimitError as e:
-              retry_after = json.loads(e.http_body)["error"]["retry_after"]
-              print(f"Rate limit reached. Retrying in {retry_after} seconds...")
-              await asyncio.sleep(retry_after)
-      self.last_request_time = time.monotonic()
-      return response
-
-
-
-
+    async def send_request(self, **kwargs):
+        while True:
+            try:
+                response = await asyncio.to_thread(openai.Completion.create, **kwargs)
+                break
+            except openai.error.RateLimitError as e:
+                retry_after = json.loads(e.http_body)["error"]["retry_after"]
+                print(f"Rate limit reached. Retrying in {retry_after} seconds...")
+                await asyncio.sleep(retry_after)
+        self.last_request_time = time.monotonic()
+        return response
